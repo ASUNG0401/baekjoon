93264154fb57362a3a510aa0699631835d64845c
ASUNG0401 <127282119+ASUNG0401@users.noreply.github.com>
2024-01-23 17:15:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 212 ms, Memory: 17532 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //sc라는 스캐너 변수 만들기
+
+        String st = sc.next(); // st라는 입력값 변수 만들기
+        System.out.println((st.length())); // st.length로 길이 측정
+        sc.close();
+//-------------------------------------------------------------------
+    }
+}
+    
+
