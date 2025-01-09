3596a5e2a884a4cca5b12c5f739d6d9ab921c282
ASUNG0401 <127282119+ASUNG0401@users.noreply.github.com>
2025-01-09 22:23:31 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 176 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        int num2 = sc.nextInt();
+        String num3 = Integer.toString(num);
+        String num4 = Integer.toString(num2);
+
+
+        System.out.println(num3+num4);
+        
+}
+}
