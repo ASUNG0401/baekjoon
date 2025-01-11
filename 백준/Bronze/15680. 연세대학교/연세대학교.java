367ee998aa80d6a043493fbd39d173913c09f1da
ASUNG0401 <127282119+ASUNG0401@users.noreply.github.com>
2025-01-12 01:40:49 +0900

    [Bronze V] Title: 연세대학교, Time: 172 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Stack<Character> stack = new Stack<>();
+
+        long num = sc.nextLong();
+        // long num2 = sc.nextLong();
+
+        if(num == 1){
+            System.out.println("Leading the Way to the Future");
+        }else{
+            System.out.println("YONSEI");
+        }
+
+
+    }
+}
