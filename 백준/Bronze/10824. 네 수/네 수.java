4b447bbc53286ce0cc34e05b0b5b62b47bab35d4
_REN_31 <127282119+ASUNG0401@users.noreply.github.com>
2025-01-26 15:10:09 +0900

    [Bronze III] Title: 네 수, Time: 172 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            String a = sc.next();
+            String b = sc.next();
+            String c = sc.next();
+            String d = sc.next();
+
+            String ab = a+b;
+            String cd = c+d;
+            System.out.println(Long.valueOf(ab)+Long.valueOf(cd));
+            // sc.nextLine();
+
+    }
+    }
