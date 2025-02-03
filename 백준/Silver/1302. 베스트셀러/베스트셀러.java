7cdef79bfd149e2049931614185e47ed65f17771
_REN_31 <127282119+ASUNG0401@users.noreply.github.com>
2025-02-04 00:49:09 +0900

    [Silver IV] Title: 베스트셀러, Time: 180 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+        Map<String,Integer> map = new HashMap<>();
+        int num =sc.nextInt();
+        ArrayList<String> list = new ArrayList<>();
+
+        int count = 0;
+        for(int i = 0; i < num; i++){
+            String book = sc.next();
+            map.put(book, map.getOrDefault(book, 0) + 1);
+        }
+
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() > count){
+                count = entry.getValue();
+            }
+        }
+
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() == count){
+                list.add(entry.getKey());
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.get(0));
+        
+	}
+}
